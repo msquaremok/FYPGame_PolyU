fdba65c9a193be09232af54216da7928a967117d
Matthew Mok <matthewmoktf@gmail.com>
2020-06-19 02:32:11 +0800

    Renewal of jump method and fall method
    1. Better fall experience (less floaty)
    2. Set Max Jump Count (limit players for infinite bouncing before releasng "space" jump
    3. Big Jump and Small Jump implemented
    
    Co-Authored-By: zaft12345678 <zaft12345678@users.noreply.github.com>

--- Final_Year_Project/Assets/Script/PlayerController.cs
+++ Final_Year_Project/Assets/Script/PlayerController.cs
@@ -7,19 +7,32 @@ public class PlayerController : MonoBehaviour
     public LayerMask groundLayer;
     Rigidbody2D rb2d;
     SpriteRenderer spriteRenderer;
-    [SerializeField] int moveSpeed;
-    [SerializeField] int jumpSpeed;
-    [SerializeField] float halfXLength;
-    [SerializeField] float halfYLength;
+    [SerializeField] float moveSpeed;
+    [SerializeField] float jumpSpeed;
+    float halfXLength; //for ground overlap check x
+    float halfYLength; //for ground overlap check y
+    [SerializeField] float fallMultiplier; // for better fall down feeling
+    [SerializeField] float minJumpTime; //check hold time for bigger jumps
+    private float jumpTimeCounter;
+    public int maxJumpCount;
+    [SerializeField] int jumpCount;
+
+    float temp;
     
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         rb2d.freezeRotation = true;
+        jumpTimeCounter = 0;
+        jumpCount = maxJumpCount;
+        halfXLength = GetComponent<BoxCollider2D>().size.x;
+        halfYLength = GetComponent<BoxCollider2D>().size.y + GetComponent<BoxCollider2D>().edgeRadius;
+        Debug.Log(halfXLength);
+        Debug.Log(halfYLength);
     }
 
-    private void FixedUpdate()
+    void Update()
     {
         if(Input.GetKey("d") || Input.GetKey("right"))
         {
@@ -33,40 +46,63 @@ public class PlayerController : MonoBehaviour
         {
             Move(0);
         }
-        if(Input.GetKey("space") && IsGrounded())
-        {            
-            Jump(jumpSpeed);
+
+        if(Input.GetKey("space"))
+        {
+            if(IsGrounded() && jumpCount > 0)
+            {
+                rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed);
+                jumpCount--;
+            }
         }
+        //When it is jumping
+        if(!IsGrounded())
+        {
+            jumpTimeCounter += Time.deltaTime;
+        }
+        // for jump span control
+        if(!Input.GetKey("space"))
+        {   
+            //Instant fall when release jump
+            if(rb2d.velocity.y > 0 && jumpTimeCounter >= minJumpTime)
+            {
+                rb2d.velocity = new Vector2(rb2d.velocity.x, 0);                                
+            }
+        }
+
+        // Reset jump count prevent over bouncing and Time Counter  
+        if(IsGrounded())
+        {
+            jumpTimeCounter = 0;
+            if(!Input.GetKey("space"))
+            {
+                jumpCount = maxJumpCount;
+            }
+        }              
     }
 
-    void Move(int moveSpeed)
+    void FixedUpdate() 
+    {
+        //better fall feeling manipulator
+        if(rb2d.velocity.y <= 0)
+        {
+            rb2d.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }    
+    }
+
+    void Move(float moveSpeed)
     {
         rb2d.velocity = new Vector2(moveSpeed, rb2d.velocity.y);
     }
 
-    void Jump(int jumpSpeed)
-    {
-        rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed);
-    }
     // Check if player touches ground
     bool IsGrounded()
     {
         bool isGrounded = Physics2D.OverlapArea(new Vector2(transform.position.x - halfXLength, transform.position.y - halfYLength),
             new Vector2(transform.position.x + halfXLength, transform.position.y - (halfYLength + 0.01f)), groundLayer);
-        /*Vector2 position = transform.position;
-        //Vector2 direction = Vector2.down;
-        float distance = 1.0f;
-        RaycastHit2D hit = Physics2D.OverlapCircle(position, distance, groundLayer);
-        if(hit.collider != null)
-        {
-            Debug.Log("Grounded");
-            return true;
-        }
-        Debug.Log("Not Grounded");
-        return false;*/
-        Debug.Log(isGrounded);
         return isGrounded;
     }
+    //Debugging for Gorund touching check
     private void OnDrawGizmos()
     {
         Gizmos.color = new Color (0, 1, 0, 0.5f);
